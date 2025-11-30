ea5463793c4a92e3127a2c3176d9a42f6f64bf4a
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-11-30 21:21:36 +0200

    Adding Order to the Admin with specific control

--- storefront/store/admin.py
+++ storefront/store/admin.py
@@ -10,7 +10,7 @@ from . import models
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
     # The list_display specify the columns that will be displayed when entering the Admin panal for the Products
-    list_display = ['title', 'unit_price', 'inventory_status']
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
 
     # The list_editable prop used to specify the fields that could be edited on the list page.
     list_editable = ['unit_price']
@@ -30,3 +30,10 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     list_per_page = 10
     ordering = ['first_name', 'last_name']
+
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'placed_at', 'customer__first_name']
+    list_select_related = ['customer']
+    ordering = ['placed_at']
