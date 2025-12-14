c01f3df0de367a322ba4e2dc2eea1fd81c0131b8
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-14 22:08:39 +0200

    Creating the product-detail end point and it routing url

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from .models import Product
+from .serializers import ProductSerializer
 # Create your views here.
 
-def product_list(request):
-    return HttpResponse("Products")
+
+@api_view(['GET'])
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    serializer = ProductSerializer(product)
+    return Response(data=serializer.data, status=status.HTTP_200_OK)
