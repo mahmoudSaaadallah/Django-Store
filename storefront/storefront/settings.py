223f28c2b6202b761609d1f1698e80f540eabd9f
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-14 22:10:58 +0200

    fixing the return of the decimal value in the end points.

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -143,3 +143,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# fixing the retur of the decimal value in the end points.
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING':False,
+}
