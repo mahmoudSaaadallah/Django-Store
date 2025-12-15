a698e5d5fb34332438f20a9f49bd1e6da7d1b550
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-15 13:14:37 +0200

    Update Serializers to use ModelSerializer instead of using tailor serializer

--- storefront/store/serializers.py
+++ storefront/store/serializers.py
@@ -1,7 +1,38 @@
+from decimal import Decimal
 from rest_framework import serializers
+from .models import Collection, Product
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    unit_price = serializers.DecimalField(max_digits=6, decimal_places=2)
+
+
+class CollectionSerializer(serializers.Serializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+class ProductSerializer(serializers.ModelSerializer):
+    # we could add a new attribute that doesn't exist in the Product model like
+    price_with_tax = serializers.SerializerMethodField(method_name='calculate_price_with_tax')
+    # Here we created a new attribute with type of serializerMethod which mean it accept a method that is responsabile for getting its vale.
+
+    # Also we could add a related field by different ways
+    # if we want to ge the id for the collection we could use 
+    # collection = serializers.PrimaryKeyRelatedField( 
+    #     queryset = Collection.objects.all()
+    # )
+    # but if we want to represent the string then we could use
+    collection = serializers.StringRelatedField()
+    # here we have to know that without using select_related('collection') in the views.py 
+    # this will case N + 1 problem.
+
+    # Ther is another way to add a related field, like if we want to return an object of the collection then we could use
+    # collection = CollectionSerializer()
+     
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+    def calculate_price_with_tax(self, product):
+        # Here we used Decimal Class as we can't mulitply float with decimla as the unit_price is decimal and 1.14 is float then 
+        # we can't mulitply them so we have to convert 1.14 to decimal before appling the operation.
+        # Also we used round to round the result to only two degits.
+        return round(product.unit_price * Decimal(1.14),2)
     
