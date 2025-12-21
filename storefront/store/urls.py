7f8a6a10069fc03a077785be483d9da24ee60b06
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-21 22:39:16 +0200

    Modified the views for all the end points and user router with nested_router for urls

--- storefront/store/urls.py
+++ storefront/store/urls.py
@@ -1,14 +1,12 @@
-from rest_framework.routers import DefaultRouter
+
 from django.urls import path, include
+from rest_framework_nested import routers
 from . import views
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
+router.register('products', viewset=views.ProductViewSet)
+router.register('collections', viewset=views.CollectionViewSet)
+products_router = routers.NestedDefaultRouter(router, 'products', lookup='product')
+products_router.register('reviews', views.ReviewViewSet, basename='product-reviews')
 router.register('reviews', views.ReviewViewSet)
-
-urlpatterns = [
-    path('products/', views.ProductList.as_view(), name='poruct-list'),
-    path('products/<int:pk>/', views.ProductDetail.as_view(), name='product-detail'),
-    path('products/<int:product_pk>/', include(router.urls)),
-    path('collections/', views.CollectionList.as_view(), name='collection-list'), 
-    path('collection/<int:pk>', views.CollectionDetail.as_view(), name='collection-detail'),
-]
+urlpatterns = router.urls + products_router.urls
--- storefront/store/views.py
+++ storefront/store/views.py
@@ -11,78 +11,71 @@ from django.db.models.aggregates import Count
 from rest_framework.views import APIView
 # Create your views here.
 
-class ProductList(APIView):
-    def get(self, request):
-        queryset = Product.objects.select_related('collection').all()
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-    
-    def post(self, request):
-        serializer = ProductSerializer(request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        
-class ProductDetail(APIView):
-    def get(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
-    
-    def put(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-        
-    def delete(self, request, pk):
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
+    def destory(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
         if product.orderitems.count() > 0:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class CollectionList(generics.ListCreateAPIView):
-    # Here we inheret from ListCreateAPIView class which inheret from generics class and some other mixinx like(mixins.ListModelMixin, mixins.CreateModelMixin).
-    # generics class inherte from the APIView and this class has some Attributes(queryset, serializer_class).
-    # You'll need to either set these attributes,
-
-    # queryset = Collection.objects.annotate(products_count=Count('products')).all()
-    # serializer_class = CollectionSerializer
-
-    # or override `get_queryset()`/`get_serializer_class()`.
-    # If you are overriding a view method, it is important that you call
-    # `get_queryset()` instead of accessing the `queryset` property directly,
-    # as `queryset` will get evaluated only once, and those results are cached
-    # for all subsequent requests.
-    def get_queryset(self):
-        return Collection.objects.annotate(products_count=Count('products')).all()
-    
-    def get_serializer_class(self):
-        return CollectionSerializer
-
-            
-class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
-
-    def delete(self, request, pk):
+    def destroy(self, request, pk):
         collection = get_object_or_404(Collection, pk=pk)
         if collection.products.count() > 0:
             return Response({'error': 'Collection contains products'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+# class CollectionList(generics.ListCreateAPIView):
+#     # Here we inheret from ListCreateAPIView class which inheret from generics class and some other mixinx like(mixins.ListModelMixin, mixins.CreateModelMixin).
+#     # generics class inherte from the APIView and this class has some Attributes(queryset, serializer_class).
+#     # You'll need to either set these attributes,
+
+#     # queryset = Collection.objects.annotate(products_count=Count('products')).all()
+#     # serializer_class = CollectionSerializer
+
+#     # or override `get_queryset()`/`get_serializer_class()`.
+#     # If you are overriding a view method, it is important that you call
+#     # `get_queryset()` instead of accessing the `queryset` property directly,
+#     # as `queryset` will get evaluated only once, and those results are cached
+#     # for all subsequent requests.
+#     def get_queryset(self):
+#         return Collection.objects.annotate(products_count=Count('products')).all()
+    
+#     def get_serializer_class(self):
+#         return CollectionSerializer
+
+            
+# class CollectionDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Collection.objects.annotate(products_count=Count('products')).all()
+#     serializer_class = CollectionSerializer
+
+#     def delete(self, request, pk):
+#         collection = get_object_or_404(Collection, pk=pk)
+#         if collection.products.count() > 0:
+#             return Response({'error': 'Collection contains products'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+#         collection.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+    
 class ReviewViewSet(ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
+
     def get_queryset(self):
-        return self.queryset.filter(product_id=self.kwargs['product_pk'])
+        return Review.objects.filter(product_id=self.kwargs['product_pk'])
+    def get_serializer_context(self):
+        return {'product_id':self.kwargs['product_pk']}
+    # def get_queryset(self):
+    #     return self.queryset.filter(product_id=self.kwargs['product_pk'])
     
-    def perform_create(self, serializer):
-        product = get_object_or_404(Product, pk=self.kwargs['product_pk'])
-        serializer.save(product=product)
+    # def perform_create(self, serializer):
+    #     product = get_object_or_404(Product, pk=self.kwargs['product_pk'])
+    #     serializer.save(product=product)
 
     
