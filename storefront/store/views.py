6ec809675cc000468a33bb2761c8885befeafa06
Mahmoud <mahmoud.saadallah73@gmail.com>
2025-12-30 20:47:03 +0200

    Adding premissions for the customer end points

--- storefront/store/views.py
+++ storefront/store/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from rest_framework import generics
@@ -149,7 +150,12 @@ class CartItemViewSet(ModelViewSet):
 class CustomerViewSet(CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = Customer.objects.select_related('user').all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
     
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        return [IsAuthenticated()]
     @action(detail=False, methods=['GET', 'PUT'])
     def me(self, request):
         if request.method == 'GET':
